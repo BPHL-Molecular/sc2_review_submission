a6e6ac644bf5a7856e06415f1ad3c1a9e7a7d879
SESchmedes <sarahschmedes@gmail.com>
2023-01-12 15:48:23 -0500

    changed sing module to apptainer

--- sbatch_sc2_fasta_sub.sh
+++ sbatch_sc2_fasta_sub.sh
@@ -11,5 +11,7 @@
 #SBATCH --output=fasta_format.%j.out
 #SBATCH --error=fasta_format.%j.err
 
+module load apptainer
+
 #Run script/command and use $SLURM_CPUS_ON_NODE 
 python sc2_fasta_for_sub.py --assem_dir assemblies/ --meta <file_name> --state_name FL_BPHL
